ff0ae27313d496f569c0fc84f4834ad25ae3a618
joe <juengs17@gmail.com>
2026-05-12 08:01:42 -0300

    feat: add createOpening handler  logic

--- handler/createOpening.go
+++ handler/createOpening.go
@@ -3,11 +3,45 @@ package handler
 import (
 	"net/http"
 
+	"github.com/JoePereiraa/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET opening",
-	})
+	request := CreateOpeningRequest{}
+
+	if err := ctx.BindJSON(&request); err != nil {
+		logger.Errorf("JSON parser error: %v", err.Error())
+
+		sendError(ctx, http.StatusBadRequest, "invalid json")
+
+		return
+	}
+
+	if err := request.Validate(); err != nil {
+		logger.Errorf("validation error: %v", err.Error())
+
+		sendError(ctx, http.StatusBadRequest, err.Error())
+
+		return
+	}
+
+	opening := schemas.Opening{
+		Role:     request.Role,
+		Company:  request.Company,
+		Location: request.Location,
+		Remote:   *request.Remote,
+		Link:     request.Link,
+		Salary:   request.Salary,
+	}
+
+	if err := db.Create(&opening).Error; err != nil {
+		logger.Errorf("error creating opening: %v", err.Error())
+
+		sendError(ctx, http.StatusInternalServerError, "failed to create opening")
+
+		return
+	}
+
+	sendSuccess(ctx, "create-opening", opening)
 }

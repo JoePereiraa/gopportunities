046335f386e627d7edbe60480cd58882ee0820b2
joe <juengs17@gmail.com>
2026-05-12 10:08:50 -0300

    docs: add documentation for create opening

--- handler/response.go
+++ handler/response.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/JoePereiraa/gopportunities/schemas"
 	"github.com/gin-gonic/gin"
 )
 
@@ -25,4 +26,14 @@ func sendSuccess(ctx *gin.Context, op string, data interface{}) {
 		"message": fmt.Sprintf("operation from handler: %s successfull", op),
 		"data":    data,
 	})
+}
+
+type ErrorResponse struct {
+	Message   string `json:"message"`
+	ErrorCode string `json:"error_code"`
+}
+
+type CreateOpeningResponse struct {
+	Message string                  `json:"message"`
+	Data    schemas.OpeningResponse `json:"data"`
 }
